3d8ac04a833a52087a3cdcd604d6e9f3bf702882
2Shiro <animetales@naver.com>
2025-02-19 10:09:03 +0900

    add SignUp

--- class_bank_v8/src/main/java/com/tenco/bank/dto/TransferDTO.java
+++ class_bank_v8/src/main/java/com/tenco/bank/dto/TransferDTO.java
@@ -1,17 +1,11 @@
 package com.tenco.bank.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
 public class TransferDTO {
     private Long amount;
     private String wAccountNumber;
     private String dAccountNumber;
-    private String password;
+    private String password; // 출금 계좌 비밀번호
 }
